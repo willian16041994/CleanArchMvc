0e0f80bb12b45610e3fd7e2d2762be4487b0b2ae
Willian Augusto Sonsin <willian.augustos7@gmail.com>
2021-08-29 18:13:26 -0300

    interfaces category and product

---
+++ CleanArchMvc.Domain/Interfaces/ICategoryRepository.cs
@@ -0,0 +1,16 @@
+﻿using System.Threading.Tasks;
+using System.Collections.Generic;
+using CleanArchMvc.Domain.Entities;
+
+namespace CleanArchMvc.Domain.Interfaces
+{
+    public interface ICategoryRepository
+    {
+        Task<IEnumerable<Category>> GetCategoriesAsync { get; set; }
+        Task<Category> GetByIdAsync(int? id);
+
+        Task<Category> CreateAsync(Category category);
+        Task<Category> UpdateAsync(Category category);
+        Task<Category> RemoveAsync(Category category);
+    }
+}
---
+++ CleanArchMvc.Domain/Interfaces/IProductRepository.cs
@@ -0,0 +1,17 @@
+﻿using System.Threading.Tasks;
+using System.Collections.Generic;
+using CleanArchMvc.Domain.Entities;
+
+namespace CleanArchMvc.Domain.Interfaces
+{
+    public interface IProductRepository
+    {
+        Task<IEnumerable<Product>> GetProductAsync { get; set; }
+        Task<Product> GetByIdAsync(int? id);
+        Task<Product> GetProductCategoryAsync(int? id);
+
+        Task<Product> CreateAsync(Product product);
+        Task<Product> UpdateAsync(Product product);
+        Task<Product> RemoveAsync(Product product);
+    }
+}
