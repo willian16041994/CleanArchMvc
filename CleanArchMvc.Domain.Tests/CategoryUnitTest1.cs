0ff960b266a5fc03094e1f7ac58851d79008abc2
Willian Augusto Sonsin <willian.augustos7@gmail.com>
2021-09-03 00:40:25 -0300

    simple unit testes for category class with xUnit and FluentAssertions

---
+++ CleanArchMvc.Domain.Tests/CategoryUnitTest1.cs
@@ -0,0 +1,53 @@
+using CleanArchMvc.Domain.Entities;
+using FluentAssertions;
+using System;
+using Xunit;
+
+namespace CleanArchMvc.Domain.Tests
+{
+    public class CategoryUnitTest1
+    {
+        [Fact(DisplayName = "Create Category with valid state")]
+        public void CreateCategory_WithValidParameters_ResultObjectValidState()
+        {
+            Action action = () => new Category(1, "Category Name");
+            action.Should()
+                .NotThrow<CleanArchMvc.Domain.Validation.DomainExceptionValidation>();
+        }
+
+        [Fact(DisplayName = "Exception with negative id")]
+        public void CreateCategory_NegativeValue_DomainExceptionInvalidId()
+        {
+            Action action = () => new Category(-1, "Category Name");
+            action.Should()
+                .Throw<CleanArchMvc.Domain.Validation.DomainExceptionValidation>()
+                .WithMessage("Invalid Id Value.");
+        }
+
+        [Fact(DisplayName = "Exception short name for name < 3 character")]
+        public void CreateCategory_ShortNameValue_DomainExceptionShortname()
+        {
+            Action action = () => new Category(1, "Ca");
+            action.Should()
+                .Throw<CleanArchMvc.Domain.Validation.DomainExceptionValidation>()
+                .WithMessage("Very short name. The name must contain at least three characters.");
+        }
+
+        [Fact(DisplayName = "Exception name required for string empty")]
+        public void CreateCategory_MissingNameValue_DomainExceptionNameRequired()
+        {
+            Action action = () => new Category(1, "");
+            action.Should()
+                .Throw<CleanArchMvc.Domain.Validation.DomainExceptionValidation>()
+                .WithMessage("Invalid name. Name is required");
+        }
+
+        [Fact(DisplayName = "Exception for name when name is null")]
+        public void CreateCategory_WithNullValueName_DomainExceptionNameRequired()
+        {
+            Action action = () => new Category(1, null);
+            action.Should()
+                .Throw<CleanArchMvc.Domain.Validation.DomainExceptionValidation>();
+        }
+    }
+}
