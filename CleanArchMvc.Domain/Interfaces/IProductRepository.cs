c7d52e3566168cbd0af874c039cbc63d9b01d039
Willian Augusto Sonsin <willian.augustos7@gmail.com>
2021-10-20 07:59:16 -0300

    implementation of interface contracts in the concrete product class

--- CleanArchMvc.Domain/Interfaces/IProductRepository.cs
+++ CleanArchMvc.Domain/Interfaces/IProductRepository.cs
@@ -6,7 +6,7 @@ namespace CleanArchMvc.Domain.Interfaces
 {
     public interface IProductRepository
     {
-        Task<IEnumerable<Product>> GetProductAsync { get; set; }
+        Task<IEnumerable<Product>> GetProductAsync();
         Task<Product> GetByIdAsync(int? id);
         Task<Product> GetProductCategoryAsync(int? id);
 
