9449c1a43ac47de7deb5ffdf525ea267db42dd26
Willian Augusto Sonsin <willian.augustos7@gmail.com>
2022-01-12 13:20:46 -0300

    Projeto InfraIoC - registrando os serviços de contexto e do repositório

--- CleanArchMvc.WebUI/Startup.cs
+++ CleanArchMvc.WebUI/Startup.cs
@@ -1,13 +1,9 @@
+using CleanArchMvc.InfraIoC;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace CleanArchMvc.WebUI
 {
@@ -23,6 +19,8 @@ namespace CleanArchMvc.WebUI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            //adiciono o serviços da camada InfraIoC, onde todos os serviços estão
+            services.AddInfrastructure(Configuration);
             services.AddControllersWithViews();
         }
 
