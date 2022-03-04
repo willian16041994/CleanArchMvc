9e7e69b73ddd7af21157ebddf98aff3ae4da56b6
Willian Augusto Sonsin <willian.augustos7@gmail.com>
2022-03-04 08:09:33 -0300

    DTOs and Mappings

---
+++ CleanArchMvc.Application/DTOs/CategoryDTO.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CleanArchMvc.Application.DTOs
+{
+    public class CategoryDTO 
+    {
+        [MinLength(3)]
+        [MaxLength(100)]
+        [Required(ErrorMessage ="The name is required.")]
+        public string Name { get; set; }
+        
+        public int Id { get; set; }
+    }
+}
